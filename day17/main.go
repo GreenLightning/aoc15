bf6afba3535bc6f28ae301ac092c04b69e0fa4f6
Green Lightning <GreenLightning.git@googlemail.com>
2019-12-08 08:42:48 +0100

    Add day 17 part two.

--- day17/main.go
+++ day17/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 )
@@ -10,29 +11,43 @@ import (
 func main() {
 	capacities := readNumbers("input.txt")
 
+	// We store combinations as bitfields inside of uints, so we can have 31
+	// containers max (the last bit is reserved for the past-the-end value).
+	if len(capacities) >= 32 {
+		panic("input too large")
+	}
+
+	end := uint(1) << uint(len(capacities))
+
+	valid := 0
+	minValid, minContainers := 0, math.MaxInt32
+	for combination := uint(0); combination < end; combination++ {
+		volume, containers := 0, 0
+		for index, capacity := range capacities {
+			if combination&(uint(1)<<uint(index)) != 0 {
+				volume += capacity
+				containers++
+			}
+		}
+		if volume == 150 {
+			valid++
+			if containers < minContainers {
+				minValid, minContainers = 1, containers
+			} else if containers == minContainers {
+				minValid++
+			}
+		}
+	}
+
 	{
 		fmt.Println("--- Part One ---")
-		fmt.Println(countCombinations(capacities, 150))
+		fmt.Println(valid)
 	}
 
 	{
 		fmt.Println("--- Part Two ---")
+		fmt.Println(minValid)
 	}
-}
-
-func countCombinations(capacities []int, volume int) int {
-	if len(capacities) == 0 {
-		if volume == 0 {
-			return 1
-		} else {
-			return 0
-		}
-	}
-	combinations := countCombinations(capacities[1:], volume)
-	if capacities[0] <= volume {
-		combinations += countCombinations(capacities[1:], volume-capacities[0])
-	}
-	return combinations
 }
 
 func readNumbers(filename string) []int {
